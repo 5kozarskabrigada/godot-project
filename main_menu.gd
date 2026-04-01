8cc6d04962c86516d1bab87c62db618a2ca80bf1
Tomris Mammadova <wiwi@Tomriss-MacBook-Air.local>
2026-04-01 13:16:30 +0300

    .

--- main_menu.gd
+++ main_menu.gd
@@ -1,17 +1,20 @@
 extends Control
 
+signal new_game_pressed(origin: String)
+signal settings_pressed(origin: String)
+signal about_pressed(origin: String)
+signal exit_pressed(origin: String)
+
 
 func _on_sandbox_btn_pressed() -> void:
-	pass # Replace with function body.
-
+	new_game_pressed.emit("main_menu")
 
 func _on_multiplayer_btn_pressed() -> void:
-	pass # Replace with function body.
-
+	settings_pressed.emit("main_menu")
 
 func _on_multiplayer_btn_2_pressed() -> void:
-	pass # Replace with function body.
+	about_pressed.emit("main_menu")
 
 
 func _on_settings_btn_pressed() -> void:
-	pass # Replace with function body.
+	exit_pressed.emit("main_menu")
